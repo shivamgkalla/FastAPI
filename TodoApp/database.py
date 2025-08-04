a7136d49dc1103ef521697d2a85f0a8b6429b8fe
Shivam <shivamgopallank@gmail.com>
2025-08-04 13:09:51 +0530

    prod database change on render

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/TodoApplicationDatabase"
+SQLALCHEMY_DATABASE_URL = "postgresql://todoapp_db_4cu2_user:S7vqXfryVM8aL9bFkArb9fhUwkDBHS0z@dpg-d2866lp5pdvs73879dag-a.singapore-postgres.render.com/todoapp_db_4cu2"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
