5a14db17ab4b7c8d23865c75490fe5e955fe61c2
Shivam <shivamgopallank@gmail.com>
2025-08-07 17:50:39 +0530

    docker caching success test commit

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -58,6 +58,13 @@ def cicd_and_docker_integration():
     return {"status": "E2E success."}
 
 
+# Docker layers caching test #
+
+@app.get("/docker_caching")
+def docker_caching_for_pip_requirements():
+    return {"status": "Check manually in the Actions."}
+
+
 app.include_router(auth.router)
 app.include_router(admin.router)
 app.include_router(users.router)
